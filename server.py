74a17c3f1a676f29657dfe9b926023b6cd4060ae
J Hari Prasad <77690054+HariPrasad1302@users.noreply.github.com>
2023-03-02 21:37:37 +0530

    nbnbn

--- server.py
+++ server.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pymongo import MongoClient
 
 app = FastAPI()
 
+class User(BaseModel):
+    name : str
+    email : str
+    password : str
+
 @app.get("/user")
 async def get_user(email: str):
-    filter ={
-        'email' : email,
-    }
-
     try:
-        client.uber.user.find_one(filter
+        filter ={
+        'email' : email,
+        }
+        project = {
+        '_id':0,
+        }
+        client.uber.user.find_one(filter=filter, project=project)
+        return True
+    except Exception as e:
+        print(str(e))
+        return False
