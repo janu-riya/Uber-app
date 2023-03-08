150b50e3c71adf3313305bbccc2511cccefbcf55
Jawaharhariharan <103201990+Jawaharhariharan@users.noreply.github.com>
2023-03-08 09:57:39 +0530

    sjjdc

--- server.py
+++ server.py
@@ -383,3 +383,16 @@ async def delete_bike(bike_model:str):
     except Exception as e:
         print(str(e))
         return False 
+
+@app.post("/bike_rc_upload")
+def bike_rc_upload(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
